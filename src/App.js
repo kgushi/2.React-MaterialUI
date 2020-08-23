fe32710fd3600f3b0218592899ee84c427542a2f
kgushi <6411668+kgushi@users.noreply.github.com>
2020-08-23 16:08:27 -0500

    Toolbar created

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from "react";
 import { Grid } from "@material-ui/core";
+import Header from "./Header";
 
 const App = () => {
   return (
     <Grid container direction="column">
-      <Grid item>This is where the header will be</Grid>
+      <Grid item>
+        <Header />
+      </Grid>
       <Grid item container>
         <Grid item xs={0} sm={2} />
-        <Grid item xs={12} sm={8}></Grid>
-        <Grid item xs={2} sm={2} />
+        <Grid item xs={12} sm={8}>
+          content
+        </Grid>
+        <Grid item xs={0} sm={2} />
       </Grid>
     </Grid>
   );
---
+++ src/Header.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import AcUnitRoundedIcon from "@material-ui/icons/AcUnitRounded";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles(() => ({
+  typographyStyles: {
+    flex: 1
+  }
+}));
+
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography className={classes.typographyStyles}>
+          This ir our header!
+        </Typography>
+        <AcUnitRoundedIcon />
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
