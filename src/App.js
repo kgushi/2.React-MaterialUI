f59a23d9f3b038167ca126a879dc1f5bc70b0572
kgushi <6411668+kgushi@users.noreply.github.com>
2020-08-23 15:42:12 -0500

    creating grid

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-import "./styles.css";
+import { Grid } from "@material-ui/core";
 
-export default function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
+    <Grid container direction="column">
+      <Grid item>This is where the header will be</Grid>
+      <Grid item container>
+        <Grid item xs={0} sm={2} />
+        <Grid item xs={12} sm={8}></Grid>
+        <Grid item xs={2} sm={2} />
+      </Grid>
+    </Grid>
   );
-}
+};
+
+export default App;
